84bd00cac8967879a087260b73b82fe55ec721f9
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-01 22:55:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,6 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name}. I am learning to program in #{language}!"
 end
 
-
 introduction("chicka-chicka-Slim-Shady")
 introduction_with_language("chicka-chicka-Slim-Shady", "freestyle")
 introduction_with_language_optional("chicka-chicka-Slim-Shady")
