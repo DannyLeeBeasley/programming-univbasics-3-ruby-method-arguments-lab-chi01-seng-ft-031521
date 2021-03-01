a690e64261e9ffd255138d4a1cea3eae959365c5
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-01 22:54:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ end
 
 introduction("chicka-chicka-Slim-Shady")
 introduction_with_language("chicka-chicka-Slim-Shady", "freestyle")
-introduction_with_language_optional("chicka-chicka-Slim-Shady", "freestyle")
+introduction_with_language_optional("chicka-chicka-Slim-Shady")
 
 
 
