3946e48da1e1641a6e404d7c6339aac81ba6b0d4
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-01 22:43:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,15 @@
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language_optional(language = "Ruby")
   puts "Hi, my name is #{name}. I am learning to rap #{language}!"
 end
+
+def introduction_with_language_optional(language = "Ruby")
+  puts "Hi, my name is #{name}. I am learning to rap #{language}!"
+end
+
 
 introduction("chicka-chicka-Slim-Shady")
 
